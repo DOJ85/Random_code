7e3d4147b2c078f9db31a3e3225ca9435dc66e2a
Unknown <florianpelloux85@gmail.com>
2018-03-17 18:57:28 +0100

    0.0.2

--- ressource/js/app.js
+++ ressource/js/app.js
@@ -7,6 +7,36 @@ function initMap() {
       lat: 34.052234,
       lng: -118.2436849
     },
-    zoom: 9
+    zoom: 9,
+    styles: [
+    {
+        "stylers": [
+            {
+                "hue": "#ff1a00"
+            },
+            {
+                "invert_lightness": true
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 33
+            },
+            {
+                "gamma": 0.5
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2D333C"
+            }
+        ]
+    }
+]
   });
 }
